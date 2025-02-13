144b0217f6ab7e2db72fc28a7e135d2756e65bd9
mgongar1508 <manologonzalezgarcia64@gmail.com>
2025-02-13 10:25:32 +0100

    Inicio

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
@@ -0,0 +1,31 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
+
+import java.util.Objects;
+
+public record Vehiculo(String marca, String modelo, String matricula) {
+
+    private static final String ER_MARCA = "^(Seat|Land Rover|KIA|Rolls-Royce|SsanYong)$";
+    private static final String ER_MATRICULA = "^[0-9]{4}[A-Z]{3}$";
+
+    public Vehiculo{
+        validarMarca(marca);
+        validarModelo(modelo);
+        validarMatricula(matricula);
+    }
+
+    private void validarMarca(String marca){
+        Objects.requireNonNull(marca, "Marca no puede ser nula");
+        if(!ER_MARCA.matches(marca)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validarModelo(String modelo){
+        Objects.requireNonNull(modelo, "Modelo no puede ser nulo");
+    }
+
+    private void validarMatricula(String matricula){
+        Objects.requireNonNull(matricula, "Matricula no puede ser nula");
+    }
+
+}
