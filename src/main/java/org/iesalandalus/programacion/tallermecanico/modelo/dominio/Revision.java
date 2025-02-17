bd0629261530db24c691b1fc4dfdd256b0d5328a
mgongar1508 <manologonzalezgarcia64@gmail.com>
2025-02-17 09:38:39 +0100

    terminado vehiculos y correccion

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Revision.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Revision.java
@@ -1,4 +1,12 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
 
 public class Revision {
+
+    private static float PRECIO_HORA;
+    private static float PRECIO_DIA;
+    private static float PRECIO_MATERIAL;
+
+
+    private Cliente cliente;
+    private Vehiculo vehiculo;
 }
--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Revisiones.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Revisiones.java
@@ -5,12 +5,5 @@ import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
 
 public class Revisiones {
 
-    private static float PRECIO_HORA;
-    private static float PRECIO_DIA;
-    private static float PRECIO_MATERIAL;
-
-
-    private Cliente cliente;
-    private Vehiculo vehiculo;
 
 }
